007c1dcbea4c0452d41b05855e4cdbd706eaa9e5
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-01 23:09:56 +0530

    Adding switch cases for division and Fibonacci Series

--- UserInput.java
+++ UserInput.java
@@ -12,6 +12,7 @@ public class UserInput{
   //Creating method handleOperations
   public void handleOperation(int choice, Calculator calculator) {
     switch (choice) {
+      //Addition
       case 1 -> {
         System.out.println("Enter two numbers:");
         double a = scan.nextDouble();
@@ -19,6 +20,7 @@ public class UserInput{
         System.out.println("Result: " + calculator.add(a, b));
       }
 
+      //Subtraction  
       case 2 -> {
         System.out.println("Enter two numbers:");
         double a = scan.nextDouble();
@@ -26,12 +28,29 @@ public class UserInput{
         System.out.println("Result: " + calculator.subtract(a, b));
       }
 
+      //Multiplication  
       case 3 -> {
         System.out.println("Enter two numbers:");
         double a = scan.nextDouble();
         double b = scan.nextDouble();
         System.out.println("Result: " + calculator.multiply(a, b));
       }
+
+      //Division  
+      case 4 -> {
+        System.out.println("Enter two numbers:");
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        System.out.println("Result: " + calculator.divide(a, b));
+      }
+
+      //Fibonacci Series
+      case 5 -> {
+        System.out.println("Enter a number:");
+        int n = scan.nextInt();
+        System.out.println("Fibonacci: " + calculator.fibonacci(n));
+      }
+
         
     }
   }
