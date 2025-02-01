84416b192f6e901d2c660f285950dcb6552e9d1c
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-01 22:52:09 +0530

    creating subtract method

--- Calculator.java
+++ Calculator.java
@@ -5,4 +5,9 @@ public class Calculator {
   public double add(double a, double b) { 
         return a + b; 
     }
+
+  //Creating method for subtraction
+  public double subtract(double a, double b) { 
+        return a - b; 
+    }
 }
