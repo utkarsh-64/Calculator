fd42e3c53940422a6892d6b41f559f8b382100c8
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-01 23:02:37 +0530

    Creating method handleOperations

--- UserInput.java
+++ UserInput.java
@@ -3,9 +3,14 @@ import java.util.* ;
 
 public class UserInput{
   private Scanner scan;
-
+  private final Scanner scan;
+  
   public UserInput(Scanner scan) {
         this.scan = scan;
     }
+
+  //Creating method handleOperations
+  public void handleOperation(int choice, Calculator calculator) {
   
+  }
 }
