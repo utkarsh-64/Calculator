7df2d88469979850c531d589627bfb55e5d5d1d4
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-02-01 23:00:59 +0530

    Creating UserInput constructor

--- UserInput.java
+++ UserInput.java
@@ -1,4 +1,11 @@
 //UserInput.java
-public class UserInput{
+import java.util.* ;
 
+public class UserInput{
+  private Scanner scan;
+
+  public UserInput(Scanner scan) {
+        this.scan = scan;
+    }
+  
 }
